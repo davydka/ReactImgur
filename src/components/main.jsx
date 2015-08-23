412e1b00e0e2243215a5905c384a7559fabde746
davydka <davydka@gmail.com>
2015-08-23 01:42:15 -0400

    Verifying main is rendered via trigger.

---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Header = require('./header');
+
+var renderCount= 0;
+
+module.exports = React.createClass({
+
+	render: function(){
+		console.log(renderCount++);
+		return <div>
+			<Header> </Header>
+			{this.props.children}
+		</div>
+	}
+})
