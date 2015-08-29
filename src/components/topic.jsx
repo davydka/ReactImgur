ef0895f814777eb8c7dde2ded08307fe315ee71e
davydka <davydka@gmail.com>
2015-08-29 13:38:31 -0400

    Getting topic image data onto topic pages.

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,8 +1,33 @@
 var React = require('react');
+var Actions = require('../actions');
+var Reflux = require('reflux');
+
+var ImageStore = require('../stores/image-store');
 
 module.exports = React.createClass({
+	mixins: [
+		Reflux.listenTo(ImageStore, 'onChange')
+	],
+
+	getInitialState: function(){
+		return {
+			images: []
+		}
+	},
+
+	componentWillMount: function(){
+		Actions.getImages(this.props.params.id);
+	},
 
 	render: function(){
-		return <div>I am a topic. {this.props.id}</div>
+		return <div>
+
+		</div>
+	},
+
+	onChange: function(event, images){
+		this.setState({
+			images: images
+		})
 	}
 });
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,7 +2,7 @@ var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = '1c7a1b9d4fbaf6c'; //i know i know i know
 
-module.exports = window.api = {
+module.exports = {
 	get: function(url){
 		return fetch(rootUrl + url, {
 			headers: {
