a3c5e33116dbc1d0145398971cd0dda756279daf
davydka <davydka@gmail.com>
2015-08-29 13:10:09 -0400

    Using some react router helpers for styling the header.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,25 @@ var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
 
+var Actions = require('../actions');
+var TopicStore = require('../stores/topic-store');
+var Reflux = require('reflux');
+
 module.exports = React.createClass({
+
+	mixins: [
+		Reflux.listenTo(TopicStore, 'onChange')
+	],
+
+	componentWillMount: function(){
+		Actions.getTopics();
+	},
+
+	getInitialState: function(){
+		return {
+			topics: []
+		}
+	},
 
 	render: function(){
 		return <nav className="navbar navbar-default header">
@@ -11,9 +29,25 @@ module.exports = React.createClass({
 					Imgur Browser
 				</Link>
 				<ul className="nav navbar-nav navbar-right">
-					<li><a href="">Topic #1</a></li>
+					{this.renderTopics()}
 				</ul>
 			</div>
 		</nav>
+	},
+
+	onChange: function(event, topics){
+		this.setState({
+			topics: topics
+		})
+	},
+
+	renderTopics: function(){
+		return this.state.topics.slice(0, 4).map(function(topic){
+			return <li key={topic.id}>
+				<Link activeClassName="active" to={'topics/' + topic.id}>
+					{topic.name}
+				</Link>
+			</li>
+		});
 	}
 })
---
+++ src/components/topic.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+	render: function(){
+		return <div>I am a topic. {this.props.id}</div>
+	}
+});
